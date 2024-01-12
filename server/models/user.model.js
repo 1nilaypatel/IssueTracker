7e2ea43d14b76aaa9e51e4c906423c50095f8f7a
1nilaypatel <nilaynilaypatelpatel@gmail.com>
2024-01-13 00:54:43 +0530

    intialised user models

---
+++ server/models/user.model.js
@@ -0,0 +1,27 @@
+import mongoose from 'moongose';
+
+const userSchema = new mongoose.Schema({
+  username:{
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email:{
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password:{
+    type: String,
+    required: true,
+  },
+  profilephoto:{
+    type: String,
+    default: "https://www.shutterstock.com/image-vector/vector-flat-illustration-grayscale-avatar-600nw-2264922221.jpg"
+  }
+}, 
+{timestamps: true});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
