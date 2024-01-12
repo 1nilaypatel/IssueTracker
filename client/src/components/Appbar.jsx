e9b90e4e6649e91f3e6ef48cbad9858dd7eb819c
1nilaypatel <nilaynilaypatelpatel@gmail.com>
2024-01-13 00:50:22 +0530

    working on Appbar

--- client/src/components/Appbar.jsx
+++ client/src/components/Appbar.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import {Link} from 'react-router-dom';
 
 export default function Appbar() {
   return (
-    <div>Appbar</div>
-  )
+    <header className='bg-black shadow-md'>
+      <div className='flex justify-between items-center p-3 max-w-6xl mx-auto'>
+        <Link to='/'>
+          <h1 className='flex fex-wrap font-bold text-sm sm:text-xl'>
+            <span className='text-slate-100'>Issue</span>
+            <span className='text-slate-400'>Tracker</span>
+          </h1>
+        </Link>
+        <ul className='flex gap-4'>
+          <Link to='/sign-in'>
+            <li className='text-slate-400 hidden sm:inline hover:underline'>
+              Log In
+            </li>
+          </Link>
+          <Link to='/sign-up'>
+          <li className='text-slate-400 hover:underline'>
+              Sign Up
+            </li>
+          </Link>
+        </ul>
+      </div>
+    </header>
+  );
 }
