b31c4ba8f0d16d1f6fcf95b8d671ae44bf898342
1nilaypatel <nilaynilaypatelpatel@gmail.com>
2024-01-13 01:09:53 +0530

    created test Server route

--- server/index.js
+++ server/index.js
@@ -1,8 +1,6 @@
 import express from 'express';
-
-const app = express();
-
 import mongoose from 'mongoose';
+import userRouter from './routes/user.route.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -12,6 +10,10 @@ mongoose.connect(process.env.MONGO).then(() => {
   console.log(err);
 });
 
+const app = express();
+
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
+
+app.use("/server/user", userRouter);
