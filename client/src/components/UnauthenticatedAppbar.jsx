575fdfa6f6a5efe0c6691603f3c0a2b94fe6bea5
1nilaypatel <nilaynilaypatelpatel@gmail.com>
2024-01-23 00:22:39 +0530

    home page done without effects

--- client/src/components/UnauthenticatedAppbar.jsx
+++ client/src/components/UnauthenticatedAppbar.jsx
@@ -4,19 +4,19 @@ export default function UnauthenticatedAppbar() {
   return (
     <div className='flex justify-between items-center p-3 max-w-6xl mx-auto'>
       <Link to='/'>
-        <h1 className='flex font-bold text-sm sm:text-xl'>
+        <h1 className='flex font-bold text-xl sm:text-2xl'>
           <span className='text-slate-100'>Issue</span>
           <span className='text-slate-400'>Tracker</span>
         </h1>
       </Link>
-      <ul className='flex gap-4'>
+      <ul className='flex flex-row justify-center items-center gap-4'>
         <Link to='/sign-in'>
           <li className='text-slate-400 hidden sm:inline hover:underline'>
             Log In
           </li>
         </Link>
         <Link to='/sign-up'>
-          <li className='text-slate-400 hover:underline'>
+          <li className='text-slate-800 hover:underline bg-indigo-400 px-2 py-1 rounded-full'>
             Sign Up
           </li>
         </Link>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 import { createIssue, filterByDueDate, filterByPriority, filterByStatus, issuesList, mobileView, notification } from '../assets/images';
 
@@ -18,15 +17,15 @@ const Home = () => {
         </Link>
       </header>
 
-      <section className="bg-gray-950 rounded-lg p-8 shadow-md mb-8 mx-14 md:mx-32">
+      <section className="bg-gray-700 bg-opacity-30 rounded-lg p-8 shadow-2xl mb-8 mx-auto text-center">
         <img
           src={issuesList}
-          alt="Create Issues Screenshot"
-          className="rounded-md shadow-md"
+          alt="Issues List img"
+          className="rounded-md shadow-2xl h-auto md:w-10/12 inline-block md:my-7"
         />
       </section>
 
-      <section className="p-8 shadow-md mb-8 mx-auto">
+      <section className="p-8 shadow-2xl mb-8 mx-auto">
         <h2 className="text-3xl md:text-5xl font-semibold mb-7">
           The issue tracker<br/>built to deliver<br/>issue-free software!
         </h2>
@@ -35,94 +34,94 @@ const Home = () => {
         </p>
       </section>
 
-      <section className="flex flex-col items-center bg-gray-950 rounded-lg p-8 shadow-md mb-8 mx-14 md:mx-32">
-        <h2 className="text-4xl md:text-6xl font-semibold mb-4">
+      <section className="flex flex-col items-center bg-slate-950 bg-opacity-30 rounded-lg p-8 shadow-2xl mb-8 mx-auto">
+        <h2 className="text-4xl md:text-6xl font-semibold mb-6">
           Issue tracking<br/>you’ll enjoy using
         </h2>
         <div className="flex items-center justify-center mb-6">
           <img
             src={createIssue}
-            alt="Create Issues Screenshot"
-            className="rounded-md shadow-md"
+            alt="Create Issue img"
+            className="rounded-md shadow-2xl h-auto md:w-7/12 md:my-7"
           />
         </div>
       </section>
 
       <div className='flex flex-col lg:flex-row gap-3 lg:gap-7 mx-14 md:mx-32'>
-        <section className="bg-gray-950 rounded-lg p-8 shadow-md mb-8 lg:w-1/2">
+        <section className="bg-gray-600 bg-opacity-50 rounded-lg p-8 shadow-2xl mb-8 lg:w-1/2">
           <h2 className="text-4xl md:text-5xl font-bold mb-4">
             Stay in the loop! 
           </h2>
-          <p className="text-gray-500 text-sm md:text-base">
+          <p className="text-gray-400 text-sm md:text-base mb-7">
           Receive notifications whenever a new task<br/> is assigned to you, keeping you<br/> informed and on top of your responsibilities.
           </p>
           <div className="flex items-center justify-center mb-6">
             <img
               src={notification}
-              alt="Create Issues Screenshot"
-              className="rounded-md shadow-md"
+              alt="Notification img"
+              className="rounded-md shadow-2xl md:h-96 lg:h-fit"
             />
           </div>
         </section>
-        <section className="bg-gray-950 rounded-lg p-8 shadow-md mb-8 lg:w-1/2">
+        <section className="bg-gray-600 bg-opacity-30 rounded-lg p-8 shadow-2xl mb-8 lg:w-1/2">
           <h2 className="text-4xl md:text-5xl font-bold mb-4">
             Make it yours
           </h2>
-          <p className="text-gray-500 text-sm md:text-base">
+          <p className="text-gray-400 text-sm md:text-base mb-7">
             Quickly apply filters to view<br/> the issues list in your<br/>suitable manner.
           </p>
-          <div className="flex items-center justify-center mb-6">
+          <div className="flex flex-col items-center justify-center mb-6 gap-4">
             <img
               src={filterByPriority}
-              alt="Create Issues Screenshot"
-              className="rounded-md shadow-md"
+              alt="Filter By Priority img"
+              className="rounded-md shadow-2xl md:h-96 lg:h-fit"
             />
-            {/* <img
+            <img
               src={filterByStatus}
-              alt="Create Issues Screenshot"
-              className="rounded-md shadow-md"
-            /> */}
+              alt="Filter By Status img"
+              className="rounded-md shadow-2xl md:h-96 lg:h-fit"
+            />
           </div>
         </section>
       </div>
 
-      <section className="flex flex-col lg:flex-row items-center bg-gray-950 rounded-lg p-8 shadow-md mb-8 mx-14 md:mx-32">
-        <div className='lg:w-2/3'>
+      <section className="flex flex-col lg:flex-row items-center bg-gray-700 rounded-lg p-8 shadow-2xl mb-8 mx-14 md:mx-32">
+        <div className='lg:w-2/3 mb-7'>
           <h2 className="text-4xl md:text-6xl font-semibold mb-4">
             Time tracking
           </h2>
-          <p className="text-gray-500 text-sm md:text-base">
-            Effortlessly manage your project timelines with our intuitive time tracking feature.<br/> Track the creation and due dates of each issue, and easily filter your tasks<br/> based on due dates to prioritize your work efficiently
+          <p className="text-gray-400 text-sm md:text-base">
+            Effortlessly manage your project timelines with our intuitive time tracking feature.<br/> Track the creation and due dates of each issue, and easily filter your tasks<br/> based on due dates to prioritize your work efficiently.
           </p>
         </div>
-        <div className="flex items-center justify-center mb-6">
+        <div className="flex items-center justify-center mb-6 lg:w-1/3">
           <img
             src={filterByDueDate}
-            alt="Create Issues Screenshot"
-            className="rounded-md shadow-md"
+            alt="Filter By Due Date img"
+            className="rounded-md shadow-2xl md:h-96 lg:h-fit lg:mr-12"
           />
         </div>
       </section>
 
-      <section className="flex flex-col lg:flex-row items-center rounded-lg p-8 shadow-md mb-8 mx-14 md:mx-32">
-        <div className="flex items-center justify-center mb-6">
+      <section className="flex flex-col lg:flex-row items-center rounded-lg p-8 shadow-2xl mb-8 md:mx-32">
+        <div className="flex items-center justify-center mb-6 lg:w-2/5 order-2 lg:order-1">
           <img
             src={mobileView}
-            alt="Create Issues Screenshot"
-            className="rounded-md shadow-md"
+            alt="Mobile View img"
+            className="rounded-md h-96 md:h-96 lg:h-fit"
           />
         </div>
-        <div className='lg:w-2/3 p-9'>
-          <h2 className="text-4xl md:text-6xl font-semibold mb-4">
+        <div className='lg:w-2/3 p-9 order-1 lg:order-2'>
+          <h2 className="text-4xl md:text-6xl font-semibold mb-7">
           Bug tracking,<br/>at your fingertips
           </h2>
-          <p className="text-gray-400">
+          <p className="text-gray-400 text-sm md:text-base">
             Experience bug tracking seamlessly, whether you're at your desk or on the go. Our responsive design ensures a smooth bug tracking experience on your desktop, tablet, or mobile device. Track and manage issues effortlessly, right at your fingertips.
           </p>
         </div>
       </section>
 
-      <section className="bg-slate-600 flex flex-col lg:flex-row items-center p-24 shadow-md mb-8 gap-20">
+      <section className="bg-slate-600 flex flex-col lg:flex-row items-center p-24 shadow-2xl mb-8 gap-20">
         <h2 className="text-4xl md:text-6xl font-semibold mb-4 ml-5">
         Swat bugs before<br/>they know what hit them!
         </h2>
