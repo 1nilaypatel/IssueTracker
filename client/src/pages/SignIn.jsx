f93983a517b312908f55febe1ddb4570babf8a1b
1nilaypatel <nilaynilaypatelpatel@gmail.com>
2024-01-23 11:00:22 +0530

    added animation in sigIn, signUp page

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -4,9 +4,12 @@ import { Link, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from 'react-redux';
 import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice';
 import OAuth from "../components/OAuth";
+import { motion } from 'framer-motion';
+import { useEffect } from "react";
 
 
 export default function SignIn() {
+  const [isFirstClick, setIsFirstClick] = useState(true);
   const [formData, setFormData] = useState({});
   const { loading, error } = useSelector((state) => state.user);
   const navigate = useNavigate();
@@ -19,24 +22,32 @@ export default function SignIn() {
     });
   };
 
+  useEffect(() => {
+    dispatch(signInFailure(null));
+  }, []);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-    try {
-      dispatch(signInStart());
-      const response = await axios.post('/server/auth/signin', formData, {
-        headers: {
-          "Content-Type": "application/json",
+    if(isFirstClick){
+      setIsFirstClick(false);
+    }else{
+      try {
+        dispatch(signInStart());
+        const response = await axios.post('/server/auth/signin', formData, {
+          headers: {
+            "Content-Type": "application/json",
+          }
+        });
+        const data = response.data;
+        if (data.success === false) {
+          dispatch(signInFailure(data.message));
+          return;
         }
-      });
-      const data = response.data;
-      if (data.success === false) {
-        dispatch(signInFailure(data.message));
-        return;
+        dispatch(signInSuccess(data));
+        navigate('/profile');
+      } catch (error) {
+        dispatch(signInFailure(error.response.data.message));
       }
-      dispatch(signInSuccess(data));
-      navigate('/profile');
-    } catch (error) {
-      dispatch(signInFailure(error.response.data.message));
     }
   };
 
@@ -45,29 +56,42 @@ export default function SignIn() {
       <h1 className='text-slate-300 text-center text-xl mb-8'>
         Log in to IssueTracker
       </h1>
-      <form onSubmit={handleSubmit} className="flex flex-col gap-5">
+      <div className="flex flex-col gap-5">
         <OAuth />
-        <input 
-          type="email"
-          placeholder="nilaypatel@gmail.com"
-          id="email"
-          className="border rounded-md p-2"
-          onChange={handleChange}
-        />
-        <input 
-          type="password"
-          placeholder="password"
-          id="password"
-          className="border rounded-md p-2"
-          onChange={handleChange}
-        />
-        <button 
+        {!isFirstClick && (
+          <motion.div 
+            className="flex flex-col gap-5"
+            initial={{ scale: 0, y: 0, }}
+            animate={{ scale:1, }}
+            transition={{ duration: 0.3, ease: "easeInOut"}}
+          >
+            <input 
+              type="email"
+              placeholder="nilaypatel@gmail.com"
+              id="email"
+              className="border rounded-md p-2"
+              required
+              onChange={handleChange}
+            />
+            <input 
+              type="password"
+              placeholder="password"
+              id="password"
+              className="border rounded-md p-2"
+              required
+              onChange={handleChange}
+            />
+          </motion.div>
+        )}
+        <motion.button 
+          onClick={handleSubmit}
           disabled={loading}
           className="bg-gray-700 text-slate-300 p-2 rounded-md hover:bg-opacity-85 disabled:bg-opacity-40"
+          layout
         >
           {loading ? "Loading..." : "Log In"}
-        </button>
-      </form>
+        </motion.button>
+      </div>
       <div className="flex gap-3 mt-5">
         <p className="text-slate-300">
           New to IssueTracker?
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -2,8 +2,10 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import OAuth from "../components/OAuth";
+import { motion } from 'framer-motion';
 
 export default function SignUp() {
+  const [isFirstClick, setIsFirstClick] = useState(true);
   const [formData, setFormData] = useState({});
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
@@ -18,26 +20,30 @@ export default function SignUp() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    try {
-      setLoading(true);
-      const response = await axios.post('/server/auth/signup', formData, {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-      const data = response.data;
-      // console.log(data);
-      if (data.success === false) {
+    if(isFirstClick){
+      setIsFirstClick(false);
+    }else{
+      try {
+        setLoading(true);
+        const response = await axios.post('/server/auth/signup', formData, {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+        const data = response.data;
+        // console.log(data);
+        if (data.success === false) {
+          setLoading(false);
+          setError(data.message);
+          return;
+        }
         setLoading(false);
-        setError(data.message);
-        return;
+        setError(null);
+        navigate('/sign-in');
+      } catch (error) {
+        setLoading(false);
+        setError(error.response.data.message);
       }
-      setLoading(false);
-      setError(null);
-      navigate('/sign-in');
-    } catch (error) {
-      setLoading(false);
-      setError(error.response.data.message);
     }
   };
 
@@ -46,36 +52,49 @@ export default function SignUp() {
       <h1 className='text-slate-300 text-center text-xl mb-8'>
         Create your IssueTracker account
       </h1>
-      <form onSubmit={handleSubmit} className="flex flex-col gap-5">
+      <div className="flex flex-col gap-5">
         <OAuth />
-        <input 
-          type="text"
-          placeholder="nilaypatel"
-          id="username"
-          className="border rounded-md p-2"
-          onChange={handleChange}
-        />
-        <input 
-          type="email"
-          placeholder="nilaypatel@gmail.com"
-          id="email"
-          className="border rounded-md p-2"
-          onChange={handleChange}
-        />
-        <input 
-          type="password"
-          placeholder="password"
-          id="password"
-          className="border rounded-md p-2"
-          onChange={handleChange}
-        />
-        <button 
+        {!isFirstClick && (
+          <motion.div 
+            className="flex flex-col gap-5"
+            initial={{ scale: 0, y: 0, }}
+            animate={{ scale:1, }}
+            transition={{ duration: 0.3, ease: "easeInOut"}}
+          >
+            <input 
+              type="text"
+              placeholder="nilaypatel"
+              id="username"
+              className="border rounded-md p-2"
+              required
+              onChange={handleChange}
+            />
+            <input 
+              type="email"
+              placeholder="nilaypatel@gmail.com"
+              id="email"
+              className="border rounded-md p-2"
+              required
+              onChange={handleChange}
+            />
+            <input 
+              type="password"
+              placeholder="password"
+              id="password"
+              className="border rounded-md p-2"
+              required
+              onChange={handleChange}
+            />
+          </motion.div>
+        )}
+        <motion.button 
+          onClick={handleSubmit}
           disabled={loading}
           className="bg-gray-700 text-slate-300 p-2 rounded-md hover:bg-opacity-85 disabled:bg-opacity-40"
         >
           {loading ? "Loading..." : "Sign Up"}
-        </button>
-      </form>
+        </motion.button>
+      </div>
       <div className="flex gap-3 mt-5">
         <p className="text-slate-300">
           Already a user!
